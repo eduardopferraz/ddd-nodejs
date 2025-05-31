7c9e12c596cb430255a914389dfeffb724fc515f
Eduardo Ferraz <edupferraz@gmail.com>
2025-05-31 16:20:19 -0300

    refactor: map properties and abstract entity creation

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,9 +1,13 @@
-import { Entity } from "../../core/entity"
+import { Entity } from "../../core/entities/entity"
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
+import { Optional } from "../../core/types/optional"
 
 interface AnswerProps {
+    authorId: UniqueEntityID
+    questionId: UniqueEntityID
     content: string
-    authorId: string
-    questionId: string
+    createdAt: Date
+    updatedAt?: Date
 }
 
 export class Answer extends Entity<AnswerProps> {
@@ -12,8 +16,16 @@ export class Answer extends Entity<AnswerProps> {
         return this.props.content
     }
 
-    constructor(props: AnswerProps, id?: string) {
-        super(props, id)
+    static create(
+    props: Optional<AnswerProps, 'createdAt'>,
+        id?: UniqueEntityID
+    ) {
+        const answer = new Answer({
+            ...props,
+            createdAt: new Date(),
+        }, id)
+
+        return answer
     }
 
 }
--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,3 +1,4 @@
+import { UniqueEntityID } from "../../core/entities/unique-entity-id"
 import { Answer } from "../entities/answer"
 import { AnswerSRepository } from "../repositories/answers-repository"
 
@@ -8,15 +9,15 @@ interface AnswerQuestionUseCaseRequest {
 }
 
 export class AnswerQuestionUseCase {
-    constructor (
+    constructor(
         private answersRepository: AnswerSRepository,
-    ) {}
+    ) { }
 
     async execute({ instructorId, questionId, content }: AnswerQuestionUseCaseRequest) {
-        const answer = new Answer({
+        const answer = Answer.create({
             content,
-            authorId: instructorId,
-            questionId,
+            authorId: new UniqueEntityID(instructorId),
+            questionId: new UniqueEntityID(questionId),
         })
 
         await this.answersRepository.create(answer)
