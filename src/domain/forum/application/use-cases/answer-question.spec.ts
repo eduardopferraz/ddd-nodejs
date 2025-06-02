aa2bb3b0430f519e3922e2eee29015739ec65a82
Eduardo Ferraz <edupferraz@gmail.com>
2025-06-02 00:27:47 -0300

    refactor: restructure folders to clean architecture pattern

--- src/domain/forum/application/use-cases/answer-question.spec.ts
+++ src/domain/forum/application/use-cases/answer-question.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from "vitest"
 import { AnswerQuestionUseCase } from "./answer-question"
 import { AnswerSRepository } from "../repositories/answers-repository"
-import { Answer } from "../entities/answer"
+import { Answer } from "../../enterprise/entities/answer"
 
 const fakeAnswersRepository: AnswerSRepository = {
     create: async (answer: Answer) => {
