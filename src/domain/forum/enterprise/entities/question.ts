b70e33875dac2110cecd8688c342ddee69b5ed50
Eduardo Ferraz <edupferraz@gmail.com>
2025-08-23 14:32:41 -0300

    feat: create question with attachments

--- src/domain/forum/enterprise/entities/question.ts
+++ src/domain/forum/enterprise/entities/question.ts
@@ -3,6 +3,7 @@ import { Slug } from "./value-objects/slug"
 import { UniqueEntityID } from "@/core/entities/unique-entity-id"
 import { Optional } from "@/core/types/optional"
 import dayjs from "dayjs"
+import { QuestionAttachment } from "./question-attachment"
 
 export interface QuestionProps {
     authorId: UniqueEntityID
@@ -10,6 +11,7 @@ export interface QuestionProps {
     title: string
     content: string
     slug: Slug
+    attachments: QuestionAttachment[]
     createdAt: Date
     updatedAt?: Date
 }
@@ -33,6 +35,10 @@ export class Question extends AggregateRoot<QuestionProps> {
 
     get slug() {
         return this.props.slug
+    }
+
+    get attachment() {
+        return this.props.attachment
     }
 
     get createdAt() {
@@ -71,7 +77,11 @@ export class Question extends AggregateRoot<QuestionProps> {
         this.touch()
     }
 
-    static create(props: Optional<QuestionProps, 'createdAt' | 'slug'>, id?: UniqueEntityID) {
+    set attachments(attachments: QuestionAttachment[]) {
+        this.props.attachments = attachments
+    }
+
+    static create(props: Optional<QuestionProps, 'createdAt' | 'slug' | 'attachments'>, id?: UniqueEntityID) {
         const question = new Question({
             ...props,
             slug: props.slug ?? Slug.createFromText(props.title),
